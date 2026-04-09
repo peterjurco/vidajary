c5b6f389fa0189ede84e634d82c085a8c199673a
Peter Sochor Jurčo <petrojurco@gmail.com>
2026-04-09 13:14:06 +0200

    chore: fix nested git repo, add Xcode project files

---
+++ vidajary/vidajary/vidajaryApp.swift
@@ -0,0 +1,32 @@
+//
+//  vidajaryApp.swift
+//  vidajary
+//
+//  Created by Vacuumlabs on 09/04/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct vidajaryApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ vidajary/vidajaryTests/vidajaryTests.swift
@@ -0,0 +1,18 @@
+//
+//  vidajaryTests.swift
+//  vidajaryTests
+//
+//  Created by Vacuumlabs on 09/04/2026.
+//
+
+import Testing
+
+struct vidajaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
